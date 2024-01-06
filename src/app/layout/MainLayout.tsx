c687b3e66bf7b2e6801c9f3cd1f3ffeb8c31ccc8
jungraejang <jungrae.jang@gmail.com>
2024-01-05 23:16:52 -0500

    added auth0

---
+++ src/app/layout/MainLayout.tsx
@@ -0,0 +1,19 @@
+"use client";
+import React, { ReactNode, useState, useEffect } from "react";
+import { useUser } from "@auth0/nextjs-auth0/client";
+
+type MainLayoutProps = {};
+
+const MainLayout: React.FC<MainLayoutProps> = () => {
+  const { user, error, isLoading } = useUser();
+  //   const [isClient, setIsClient] = useState(false);
+  console.log("user", user, isLoading);
+
+  return (
+    <div>
+      <h1>Main Layout</h1>
+    </div>
+  );
+};
+
+export default MainLayout;
